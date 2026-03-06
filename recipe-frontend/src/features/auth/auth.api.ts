4d2da7acb66faca6992207586e3d85e2b7315924
khachatur Saponjyan <pizzabeach4@gmail.com>
2026-03-06 23:05:25 +0300

    feat: add authentication flow with login and registration

---
+++ recipe-frontend/src/features/auth/auth.api.ts
@@ -0,0 +1,16 @@
+import { api } from "@/api/axios";
+import type {
+    AuthResponse,
+    LoginPayload,
+    RegisterPayload,
+} from "./auth.types";
+
+export async function loginUser(payload: LoginPayload) {
+    const res = await api.post<AuthResponse>("/auth/local", payload);
+    return res.data;
+}
+
+export async function registerUser(payload: RegisterPayload) {
+    const res = await api.post<AuthResponse>("/auth/local/register", payload);
+    return res.data;
+}
